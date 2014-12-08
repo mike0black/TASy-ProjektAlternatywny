9f5d2b20ee0ccae8b255f480e1e020865d206122
Antoni Borys <mike0black@gmail.com>
2014-12-08 17:33:57 +0100

    Wersja Rozwojowa NIEdziałająca
    wrzucam coby była jakbym znowyu coś zjebał w kodzie

---
+++ Sprzedane/SprzedaneWebMVC/Controllers/WyszukiwarkaController.cs
@@ -0,0 +1,31 @@
+﻿using SprzedaneWebMVC.SprzedaneReference;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SprzedaneWebMVC.Controllers
+{
+    public class WyszukiwarkaController : Controller
+    {
+        //
+        // GET: /Wyszukiwarka/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Wyszukiwarka/PoNazwie
+        public ActionResult PoNazwie(string name)
+        {
+            SprzedaneServiceClient Service = new SprzedaneServiceClient();
+            IList<Przedmioty> Lista = Service.GetPrzedmioty(name);
+            ViewBag.Lista = Lista;
+
+            return View();
+        }
+	}
+}
