1dfb48a397d360b88461a5a1d264c5a9f8245bfb
Antoni Borys <mike0black@gmail.com>
2014-12-08 16:36:22 +0100

    Naprawiłem bazę danych
    Wersja pośrednia - wieczorem wleci faktyczny postęp albocoś koło tego

--- Sprzedane/Sprzedane/SprzedaneModel.Designer.cs
+++ Sprzedane/Sprzedane/SprzedaneModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'J:\TASy\Projekt alternatywny\Sprzedane\Sprzedane\SprzedaneModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\Antoni\TASy\TASy-ProjektAlternatywny\Sprzedane\Sprzedane\SprzedaneModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
