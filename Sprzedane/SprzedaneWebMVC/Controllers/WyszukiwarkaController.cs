c65a37569f1f4c239a3fa8578ae726f6aa22f2c3
Antoni Borys <mike0black@gmail.com>
2014-12-26 14:21:14 +0100

    Wersja Rozwojowa pół-działająca
    wyszukiwanie działa jak odpytywać przez "?name="
    do poprawki

--- Sprzedane/SprzedaneWebMVC/Controllers/WyszukiwarkaController.cs
+++ Sprzedane/SprzedaneWebMVC/Controllers/WyszukiwarkaController.cs
@@ -1,11 +1,16 @@
-﻿using SprzedaneWebMVC.SprzedaneReference;
+﻿using Newtonsoft.Json;
+using SprzedaneWebMVC.Models;
+using SprzedaneWebMVC.SprzedaneReference;
 using System.Collections.Generic;
+using System.Net;
 using System.Web.Mvc;
 
 namespace SprzedaneWebMVC.Controllers
 {
     public class WyszukiwarkaController : Controller
     {
+        readonly string SprzedaneServiceUri = "http://localhost:1622/SprzedaneService.svc/";
+        
         //
         // GET: /Wyszukiwarka/
         public ActionResult Index()
@@ -17,10 +22,18 @@ namespace SprzedaneWebMVC.Controllers
         // GET: /Wyszukiwarka/PoNazwie
         public ActionResult PoNazwie(string name)
         {
-            SprzedaneServiceClient Service = new SprzedaneServiceClient();
-            IList<Przedmioty> Lista = Service.GetPrzedmioty(name);
-            ViewBag.Lista = Lista;
+            //SprzedaneServiceClient Service = new SprzedaneServiceClient();
+            //IList<Przedmioty> Lista = Service.GetPrzedmioty(name);
+            List<Przedmiot> Lista = new List<Przedmiot>();
 
+            using (WebClient webClient = new WebClient())
+            {
+                string dwml;
+                dwml = webClient.DownloadString(SprzedaneServiceUri + "przedmioty/" + name);
+                Lista = JsonConvert.DeserializeObjectAsync<List<Przedmiot>>(dwml).Result;
+            }
+
+            ViewBag.Lista = Lista;
             return View();
         }
 	}
---
+++ Sprzedane/SprzedaneWebMVC/Models/Przedmiot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SprzedaneWebMVC.Models
+{
+    public class Przedmiot
+    {
+        public int ID { get; set; }
+        public String Nazwa { get; set; }
+        public String Kategoria { get; set; }
+        public double Cena { get; set; }
+        public DateTime DataZakonczenia { get; set; }
+        public String Wystawiajacy { get; set; }
+    }
+}
